18756469a12204f90cf85dee6d2d96ff51dbe21c
Icaro de Oliveira <icaroferreira27@gmail.com>
2025-08-17 12:43:17 -0300

    Add initial HTML, CSS, and JavaScript files for password generator application

---
+++ script.js
@@ -0,0 +1,10 @@
+function gerarSenha(tamanho = 12) {
+  const caracteres =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let senha = "";
+  for (let i = 0; i < tamanho; i++) {
+    const index = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[index];
+  }
+  document.getElementById("senha").innerText = senha;
+}
